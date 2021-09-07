a9f72713f17e58f8daf80b59fcda871c27601ed7
MarioSalinas <mario_1605@live.com.mx>
2021-09-07 09:00:56 -0500

    feat: list of fights and routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Layout } from './components/Layout';
 import { Landing } from './containers/Landing';
+import { ListOfFlights } from './containers/ListOfFlights';
 
 import '../src/assets/styles/App.scss';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Layout>
-        <Landing />
+        <Switch>
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/flights" component={ListOfFlights} />
+        </Switch>
       </Layout>
-    </div>
+    </BrowserRouter>
   );
 }
 
